658918cc5df207d6379d3eacd8faf3f8db50c593
Vybes2 <oganthony82@gmail.com>
2024-05-21 21:54:20 -0400

    d

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,32 +16,27 @@ function off() {
     overflow.style.overflow = "";
 }
 
-function submit(event) {
+document.getElementById('orderForm').addEventListener('submit', function(event) {
     event.preventDefault();
-
 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const senderMessage = document.getElementById("message").value;
     const dateTime = new Date().toLocaleString();
-    console.log(name, email, phone);
-    const webhook = "https://discord.com/api/webhooks/1242371903306600518/j0spX6ZDGj09BlI-1aK-X6XdpdyAvbRgch86GvnNTgGHvKg_6F19dr9B5_R6MXdgidPT";
-
 
     const contents = {
-        content: "New Order From " + name + " at " + dateTime + "<@&1242651341235683458>",
+        content: `New Order From ${name} at ${dateTime}`,
         embeds: [
             {
                 title: "Order Details",
                 fields: [
                     { name: 'Name', value: name },
-                    // { name: 'Discord', value: senderDiscord },
                     { name: 'Email', value: email },
                     { name: 'Phone Number', value: phone },
                     { name: 'Message', value: senderMessage },
                     { name: 'Date and Time', value: dateTime },
-                  ],
+                ],
             }
         ]
     };
@@ -51,23 +46,20 @@ function submit(event) {
     fetch(webhookUrl, {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(contents)
     })
     .then(response => {
         if (response.ok) {
-        alert('Message sent successfully');
-        window.location.href = 'order.html';
+            alert('Message sent successfully');
+            window.location.href = 'order.html';
         } else {
-        alert('An unknown error occurred on the server.');
+            alert('An unknown error occurred on the server.');
         }
     })
     .catch(error => {
         console.error('Error:', error);
         alert('An unknown error occurred on the server.');
     });
-}
-
-console.log(submit())
-
+});
