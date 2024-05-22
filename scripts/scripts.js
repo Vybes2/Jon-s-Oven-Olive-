ab58ae563fbe9321d23a1d78b4800d7741ab9b2d
Vybes2 <oganthony82@gmail.com>
2024-05-21 21:46:25 -0400

    more changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,29 +16,56 @@ function off() {
     overflow.style.overflow = "";
 }
 
-function orderSubmit() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+function sendWebhook(event) {
+    event.preventDefault();
 
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const senderMessage = document.getElementById("message").value;
+    const dateTime = new Date().toLocaleString();
+    console.log(name, email, phone);
     const webhook = "https://discord.com/api/webhooks/1242371903306600518/j0spX6ZDGj09BlI-1aK-X6XdpdyAvbRgch86GvnNTgGHvKg_6F19dr9B5_R6MXdgidPT";
 
+
     const contents = {
-        "content": "New Order From " + name,
-        "embeds": [
+        content: "New Order From " + name + " at " + dateTime + "<@&1242651341235683458>",
+        embeds: [
             {
-                "title": "Order Details",
-                "description": "Name: " + name + "\nEmail: " + email + "\nPhone: " + phone,
-                "color": 16711680
+                title: "Order Details",
+                fields: [
+                    { name: 'Name', value: name },
+                    // { name: 'Discord', value: senderDiscord },
+                    { name: 'Email', value: email },
+                    { name: 'Phone Number', value: phone },
+                    { name: 'Message', value: senderMessage },
+                    { name: 'Date and Time', value: dateTime },
+                  ],
             }
         ]
     };
 
-    const request = new XMLHttpRequest();
-    request.open("POST", webhook);
-    request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-        content: contents
-    }
-    request.send(JSON.stringify(params));
+    const webhookUrl = 'https://discord.com/api/webhooks/1242371903306600518/j0spX6ZDGj09BlI-1aK-X6XdpdyAvbRgch86GvnNTgGHvKg_6F19dr9B5_R6MXdgidPT';
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contents)
+    })
+    .then(response => {
+        if (response.ok) {
+        alert('Message sent successfully');
+        window.location.href = 'order.html';
+        } else {
+        alert('An unknown error occurred on the server.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An unknown error occurred on the server.');
+    });
 }
+
